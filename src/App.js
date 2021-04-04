0535b273020e24cda13491bc20fc797c81655c95
Tatiana Rak <raktania3@gmail.com>
2021-04-04 23:41:53 +0300

    commited component Results in App

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
           <Route path={routes.MAIN_VIEW} component={MainView} />
         </Switch>
       </Suspense>
-      <Results />
+      {/* <Results /> */}
       <Footer />
     </BrowserRouter>
   );
