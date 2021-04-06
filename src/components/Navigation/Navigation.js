f855cbcdea0943da8dd6c8c075e9b8bae3d2f845
Andriy Kikot <andray838@gmail.com>
2021-04-07 01:07:10 +0300

    added completed visual part of header

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,19 @@
+import { useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import routes from '../../routes';
-// import UserInfo from '../UserInfo';
+import { ReactComponent as SignOutIcon } from '../../assets/icons/svg/signOut.svg';
+import { authOperations } from '../../redux/auth';
+import UserInfo from '../UserInfo';
+// import UserAvatarUpdate from '../UserAvatarUpdate';
 import styles from './Navigation.module.css';
 
 function Navigation({ isOpen, onOpenMobileMenu }) {
+  const dispatch = useDispatch();
   return (
-    <nav className={isOpen ? styles.navMenuActive : styles.navMenu}>
-      {/* isLoggedIn && */}
-      {/* <NavLink
+    <div className={styles.container}>
+      <nav className={isOpen ? styles.navMenuActive : styles.navMenu}>
+        {/* isLoggedIn && */}
+        {/* <NavLink
         to={routes.CONTACTS_VIEW}
         exact
         className={styles.link}
@@ -16,38 +22,52 @@ function Navigation({ isOpen, onOpenMobileMenu }) {
       >
         <span className={styles.text}>Contacts</span>
       </NavLink> */}
+        <div className={styles.wrapper}>
+          <NavLink
+            to={routes.MAIN_VIEW}
+            exact
+            className={styles.link}
+            activeClassName={styles.activeLink}
+            onClick={() => onOpenMobileMenu(false)}
+          >
+            <span className={styles.text}>Home</span>
+          </NavLink>
 
-      <NavLink
-        to={routes.MAIN_VIEW}
-        exact
-        className={styles.link}
-        activeClassName={styles.activeLink}
-        onClick={() => onOpenMobileMenu(false)}
-      >
-        <span className={styles.text}>Home</span>
-      </NavLink>
+          <NavLink
+            to={routes.USEFUL_INFO_VIEW}
+            exact
+            className={styles.link}
+            activeClassName={styles.activeLink}
+            onClick={() => onOpenMobileMenu(false)}
+          >
+            <span className={styles.text}>Materials</span>
+          </NavLink>
 
-      <NavLink
-        to={routes.USEFUL_INFO_VIEW}
-        exact
-        className={styles.link}
-        activeClassName={styles.activeLink}
-        onClick={() => onOpenMobileMenu(false)}
-      >
-        <span className={styles.text}>Materials</span>
-      </NavLink>
+          <NavLink
+            to={routes.CONTACTS_VIEW}
+            exact
+            className={styles.link}
+            activeClassName={styles.activeLink}
+            onClick={() => onOpenMobileMenu(false)}
+          >
+            <span className={styles.text}>Contacts</span>
+          </NavLink>
+        </div>
 
-      <NavLink
-        to={routes.CONTACTS_VIEW}
-        exact
-        className={styles.link}
-        activeClassName={styles.activeLink}
-        onClick={() => onOpenMobileMenu(false)}
-      >
-        <span className={styles.text}>Contacts</span>
-      </NavLink>
-      {/* <UserInfo /> */}
-    </nav>
+        <NavLink to={routes.AUTH_VIEW} onClick={() => onOpenMobileMenu(false)}>
+          <SignOutIcon
+            className={styles.signOutBtn}
+            width="16px"
+            height="16px"
+            onClick={() => dispatch(authOperations.logOut())}
+          />
+        </NavLink>
+      </nav>
+      <UserInfo onOpenMobileMenu={onOpenMobileMenu} />
+      {/* <NavLink to={routes.AUTH_VIEW} onClick={() => onOpenMobileMenu(false)}>
+        <SignOutIcon className={styles.signOutBtnLg} width="16px" height="16px" onClick={() => dispatch(authOperations.logOut())} />
+      </NavLink> */}
+    </div>
   );
 }
 
