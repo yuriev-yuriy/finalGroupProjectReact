22a2f18c64709e6ee72616734acd1fcb07f87a38
Andriy Kikot <andray838@gmail.com>
2021-04-08 00:47:19 +0300

    added redux logic for header and navigation components

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,48 +1,66 @@
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+
 import routes from '../../routes';
-import { ReactComponent as SignOutIcon } from '../../assets/icons/svg/signOut.svg';
-import { authOperations } from '../../redux/auth';
+import { authSelectors, authOperations } from '../../redux/auth';
 import UserInfo from '../UserInfo';
 // import UserAvatarUpdate from '../UserAvatarUpdate';
+import { ReactComponent as SignOutIcon } from '../../assets/icons/svg/signOut.svg';
 import styles from './Navigation.module.css';
 
 function Navigation({ isOpen, onOpenMobileMenu }) {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
   const dispatch = useDispatch();
+
   return (
     <div className={styles.container}>
       <nav className={isOpen ? styles.navMenuActive : styles.navMenu}>
-        {/* isLoggedIn && */}
-        {/* <NavLink
-        to={routes.CONTACTS_VIEW}
-        exact
-        className={styles.link}
-        activeClassName={styles.activeLink}
-        onClick={() => onOpenMobileMenu(false)}
-      >
-        <span className={styles.text}>Contacts</span>
-      </NavLink> */}
-        <div className={styles.wrapper}>
-          <NavLink
-            to={routes.MAIN_VIEW}
-            exact
-            className={styles.link}
-            activeClassName={styles.activeLink}
-            onClick={() => onOpenMobileMenu(false)}
-          >
-            <span className={styles.text}>Home</span>
-          </NavLink>
+        {isLoggedIn ? (
+          <>
+            <div className={styles.wrapper}>
+              <NavLink
+                to={routes.MAIN_VIEW}
+                exact
+                className={styles.link}
+                activeClassName={styles.activeLink}
+                onClick={() => onOpenMobileMenu(false)}
+              >
+                <span className={styles.text}>Home</span>
+              </NavLink>
 
-          <NavLink
-            to={routes.USEFUL_INFO_VIEW}
-            exact
-            className={styles.link}
-            activeClassName={styles.activeLink}
-            onClick={() => onOpenMobileMenu(false)}
-          >
-            <span className={styles.text}>Materials</span>
-          </NavLink>
+              <NavLink
+                to={routes.USEFUL_INFO_VIEW}
+                exact
+                className={styles.link}
+                activeClassName={styles.activeLink}
+                onClick={() => onOpenMobileMenu(false)}
+              >
+                <span className={styles.text}>Materials</span>
+              </NavLink>
 
+              <NavLink
+                to={routes.CONTACTS_VIEW}
+                exact
+                className={styles.link}
+                activeClassName={styles.activeLink}
+                onClick={() => onOpenMobileMenu(false)}
+              >
+                <span className={styles.text}>Contacts</span>
+              </NavLink>
+            </div>
+            <NavLink
+              to={routes.AUTH_VIEW}
+              onClick={() => onOpenMobileMenu(false)}
+            >
+              <SignOutIcon
+                className={styles.signOutBtn}
+                width="16px"
+                height="16px"
+                onClick={() => dispatch(authOperations.logOut())}
+              />
+            </NavLink>
+          </>
+        ) : (
           <NavLink
             to={routes.CONTACTS_VIEW}
             exact
@@ -52,18 +70,9 @@ function Navigation({ isOpen, onOpenMobileMenu }) {
           >
             <span className={styles.text}>Contacts</span>
           </NavLink>
-        </div>
-
-        <NavLink to={routes.AUTH_VIEW} onClick={() => onOpenMobileMenu(false)}>
-          <SignOutIcon
-            className={styles.signOutBtn}
-            width="16px"
-            height="16px"
-            onClick={() => dispatch(authOperations.logOut())}
-          />
-        </NavLink>
+        )}
       </nav>
-      <UserInfo onOpenMobileMenu={onOpenMobileMenu} />
+      {isLoggedIn && <UserInfo onOpenMobileMenu={onOpenMobileMenu} />}
     </div>
   );
 }
