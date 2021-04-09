e577ed5b264ae4e07d2661ab72c1d47940b81062
Tatiana Rak <raktania3@gmail.com>
2021-04-09 07:15:43 +0300

    added diagram

--- src/components/Results/Chart/Chart.js
+++ src/components/Results/Chart/Chart.js
@@ -21,8 +21,6 @@ const options = {
     display: true,
   },
   rotation: 0.2 * Math.PI - (25 / 180) * Math.PI,
-  // maintainAspectRatio: false,
-  // responsive: true,
   tooltips: {
     mode: 'point',
     callbacks: {
@@ -33,7 +31,7 @@ const options = {
     },
   },
 };
-const ProgressBar = ({ correctAnswers = 10, allAnswers = 12 }) => {
+const Diagram = ({ correctAnswers = 10, allAnswers = 12 }) => {
   const chartCorrect = Math.ceil((correctAnswers * 100) / allAnswers);
   const chartInCorrect = 100 - chartCorrect;
   // const chartInCorrect = allAnswers - correctAnswers;
@@ -49,26 +47,5 @@ const ProgressBar = ({ correctAnswers = 10, allAnswers = 12 }) => {
     </div>
   );
 };
-// ProgressBar.defaultProps = {
-//   correctAnswers: 11,
-// };
-export default ProgressBar;
-// import { Bar } from 'react-chartjs-2';
-// import { Doughnut } from 'react-chartjs-2';
-// import { Chart, registerables } from 'chart.js';
-//
 
-// const BarDash = () => {
-//     return (<div>
-//         hello
-//         <Doughnut
-//             data={{
-//                 labels:['Blue','Yellow']
-//             }}
-
-//             type= 'doughnut'
-
-//             />
-//     </div>)
-// }
-// export default BarDash;
+export default Diagram;
--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,26 +1,30 @@
-// import { useSelector, useDispatch } from 'react-redux';
-// import { resultOperations, resultSelectors } from '../../redux/results';
+import { useHistory, useLocation } from 'react-router-dom';
 import s from './Results.module.css';
 import cat176 from '../../assets/images/cat176.png';
 import cat120 from '../../assets/images/cat120.png';
 import defaultImg from '../../assets/images/cat120default.png';
-import ProgressBar from './Chart/Chart.js';
+import Diagram from './Chart/Chart.js';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { getRightAnswers } from '../../redux/results/result-selectors';
 
 const Results = () => {
   // const dispatch = useDispatch();
-  // const rightAnswers = useSelector(resultSelectors.getRightResults);
-  // const wrongAnswers = useSelector(resultSelectors.getWrongResults);
-  // const totalAnswers = useSelector(resultSelectors.getTotalQuestions);
-  //  const results = useSelector(resultSelectors.getResults);
+  const history = useHistory();
+  const location = useLocation();
+
+  const handleTryAgane = () => {
+    // console.log(location.state.from);
+    history.push(location.state.from);
+  };
 
   return (
     <section>
       <div className={s.container}>
         <h2 className={s.sectionTitle}>Results</h2>
         <h3 className={s.sectionSubtitle}>[TESTING THEOSY_]</h3>
-        <ProgressBar />
+        <Diagram />
         <div className={s.resultsWrapper}>
-          <p className={s.resultData}>Current answers- </p>
+          <p className={s.resultData}>Current answers- 10</p>
           <p className={s.resultData}>Total questions- 12</p>
         </div>
         <picture>
@@ -40,7 +44,13 @@ const Results = () => {
         <p className={s.testResultDescription}>
           But you still need to learn some materials.
         </p>
-        <button className={s.sectionButton}>Try again</button>
+        <button
+          type="button"
+          onClick={handleTryAgane}
+          className={s.sectionButton}
+        >
+          Try again
+        </button>
       </div>
     </section>
   );
