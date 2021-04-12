284b478dbdad60fde03766442a6f9755fe4f1746
Rostyslav Zhabinskyi <zhabinskiirostislav@gmail.com>
2021-04-12 10:22:18 +0300

    a

--- src/App.js
+++ src/App.js
@@ -6,24 +6,24 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Header from './components/Header/';
 
-// import AuthView from './views/AuthView/';
+import AuthView from './views/AuthView/';
 
 import Footer from './components/Footer/';
-// import { authOperations } from './redux/auth/';
-// import PrivateRoute from './components/Routes/PrivateRoute';
-// import PublicRoute from './components/Routes/PublicRoute';
+import { authOperations } from './redux/auth/';
+import PrivateRoute from './components/Routes/PrivateRoute';
+import PublicRoute from './components/Routes/PublicRoute';
 
 const MainView = lazy(() =>
   import('./views/MainView' /*webpackChunkName: "MainView"*/),
 );
 
-// const UseFulInfoView = lazy(() =>
-//   import('./views/UseFulInfoView' /*webpackChunkName: "UseFulInfoView"*/),
-// );
+const UseFulInfoView = lazy(() =>
+  import('./views/UseFulInfoView' /*webpackChunkName: "UseFulInfoView"*/),
+);
 
-// const ContactsView = lazy(() =>
-//   import('./views/ContactsView' /*webpackChunkName: "ContactsView"*/),
-// );
+const ContactsView = lazy(() =>
+  import('./views/ContactsView' /*webpackChunkName: "ContactsView"*/),
+);
 
 const Results = lazy(() =>
   import('./components/Results' /*webpackChunkName: "Results"*/),
@@ -31,10 +31,6 @@ const Results = lazy(() =>
 
 const TestPage = lazy(() =>
   import('./components/TestPage' /*webpackChunkName: "TestPage"*/),
-);
-
-const AuthView = lazy(() =>
-  import('./views/AuthView' /*webpackChunkName: "AuthView"*/),
 );
 
 function App() {
@@ -51,11 +47,10 @@ function App() {
           <Route exact path={routes.TEST_VIEW} component={TestPage} />
           <Route exact path={routes.MAIN_VIEW} component={MainView} />
           <Route exact path={routes.RESULT_VIEW} component={Results} />
-          <Route exact path={routes.AUTH_VIEW} component={AuthView} />
+          {/* <Route exact path={routes.AUTH_VIEW} component={AuthView} /> */}
 
           {/*           
           <PrivateRoute path={routes.USEFUL_INFO_VIEW}>
-
             <UseFulInfoView />
           </PrivateRoute>
           <PublicRoute path={routes.CONTACTS_VIEW}>
